e13d19740ad1b0bd080984f2260c70fc37922e3a
Manuel Parra <manuel.parra1@gmail.com>
2023-02-17 11:22:47 -0600

    dot py

---
+++ continuous_probabilistic_methods.py
@@ -0,0 +1,17 @@
+def get_lower_and_upper_bounds(series,multiplier):
+    Q3 = np.quantile(series, 0.75)
+    Q1 = np.quantile(series, 0.25)
+    iqr = Q3 - Q1
+    
+    upper_bound = Q3+(multiplier*iqr)
+    lower_bound = Q1-(multiplier*iqr)
+    
+    return lower_bound, upper_bound
+
+def isolate_outliers(df):
+    SD = df.std()
+    norm_median = df.median()
+    
+    outside_of_three_sd = df[(df <= (norm_median-(SD*3))) & (df >= (norm_median+(SD*3)))]
+    
+    return outside_of_three_sd
